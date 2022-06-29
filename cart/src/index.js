13fcc2c8a8e462e341f4e890b70ddf42ea513628
Tomer Shafir <tomershafir@Tomers-MacBook-Pro-5.local>
2022-06-29 19:53:19 +0300

    add shared module to webpack

--- cart/src/index.js
+++ cart/src/index.js
@@ -1,5 +1,5 @@
 import faker from 'faker'
 
-const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`
+const cartText = `<div>You have ${faker.datatype.number()} items in your cart</div>`
 
 document.querySelector('#dev-cart').innerHTML = cartText;
